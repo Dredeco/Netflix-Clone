f9e058c44d6b9ba8723845b640ef98d58946a5f5
Dredeco <86372171+Dredeco@users.noreply.github.com>
2022-06-20 17:48:59 -0300

    ...

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,10 @@
+import imgBanner from "../assets/banner.jpg"
+
 export function Banner() {
     return (
-        <div className="">
-            <div className="relative w-full h-screen bg-cover bg-banner play bg-no-repeat" />
-        </div>
+            <div className="relative w-full h-screen">
+                <div className="absolute w-full h-full bg-gradient-to-t from-black" />
+                <img src={imgBanner} className="w-full h-full z-[0]" />
+            </div>
     )
 }
